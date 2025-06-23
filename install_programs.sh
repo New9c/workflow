c3ae430acb4fdf5674e029abbf0cdf9b0c45d86b
9c <ericgwhuang@gmail.com>
2025-06-23 19:56:57 +0800

    fix things

--- install_programs.sh
+++ install_programs.sh
@@ -2,16 +2,18 @@ echo "We shall now install the most important apps, they kinda need to exist"
 sleep 2
 echo ""
 echo ""
-cp fcitx5 /usr/bin/
+sudo cp fcitx5 /usr/bin/
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk xdg-desktop-portal-gnome xwayland-satellite swaybg swaylock waybar rofi-wayland kitty fish niri yay obs-studio sddm nvim thunar
 yay -S --noconfirm webcord
 
-clear
 echo "I also like other apps, but if you don't want them, just say no"
 echo ""
 echo ""
-sudo pacman -S --needed gimp spotify
-clear
+sudo pacman -S --needed gimp 
+yay -S --needed spotify
 
 echo "Rebooting now, see you soon!"
 echo -n "2..."
