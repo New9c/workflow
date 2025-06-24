0d9b005cf20b8a57c060eddd0e41b140ba32e9bc
9c <ericgwhuang@gmail.com>
2025-06-25 05:03:25 +0800

    arf

--- sddm.sh
+++ sddm.sh
@@ -1,2 +1,4 @@
-cp -r sddm/Corners/* /usr/share/sddm/themes/Corners
-cp -r sddm/sddm.conf.d/* /etc/sddm.conf.d
+sudo mkdir -p /usr/share/sddm/themes/Corners
+sudo cp -r sddm/Corners/* /usr/share/sddm/themes/Corners
+sudo mkdir -p /etc/sddm.conf.d
+sudo cp -r sddm/sddm.conf.d/* /etc/sddm.conf.d
